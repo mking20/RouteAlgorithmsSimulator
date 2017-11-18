31f189f50eb2614928128320b7a0f61ea0dd3ff3
Fernando Raul Cortez Chavez <fernandoraulcc@gmail.com>
2017-11-18 13:13:12 -0800

    Create BellmanFord.py

---
+++ BellmanFord.py
@@ -0,0 +1,7 @@
+"""This function receives a two-dimensional list of 0's and 1's that represents a square maze, a tuple (row, column)
+for the starting tile and a tuple(row, column) for the goal tile, and a boolean variable predermined to True indicating
+if one can travel diagonally between two adjacent tiles or not. It returns a list of tuples (row, column) of all
+the tiles that form the path between the start and the goal; None is returned if no path is found. """
+
+def bellmanFord(maze, startTuple, endTuple, diagonalsAllowed=True):
+    return """List of tuples of the path's tiles """
